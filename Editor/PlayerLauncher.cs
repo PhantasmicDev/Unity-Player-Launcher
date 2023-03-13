2dd1aafc49e300ce94a0250accc6ef35c99ad3b7
PhantasmicDev <oscar.phantasmic@protonmail.com>
2023-03-12 18:30:03 -0700

    Slimmed down Player Launcher
    Player Launcher now only handles validating a launch and calling the appropriate launcher

--- Editor/PlayerLauncher.cs
+++ Editor/PlayerLauncher.cs
@@ -1,69 +1,27 @@
-using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.IO;
 using UnityEditor;
-using UnityEditor.Build;
-using UnityEditor.Build.Reporting;
-using UnityEditor.Callbacks;
-using UnityEngine;
-using Debug = UnityEngine.Debug;
 
 namespace PhantasmicGames.PlayerLauncherEditor
 {
-    [InitializeOnLoad]
-    public class PlayerLauncher : IPreprocessBuildWithReport
+    public static class PlayerLauncher
     {
-        private static Dictionary<BuildTargetGroup, List<Type>> s_launchers = new Dictionary<BuildTargetGroup, List<Type>>();
-
-        private static BuildReport s_buildReport;
-        
-        public int callbackOrder => 0;
-
-
-        static PlayerLauncher()
-        {
-            //LaunchPlayerAttribute attr = null;
-            //ar group = attr.TargetGroup;
-        }
-        
-        
-        public void OnPreprocessBuild(BuildReport report)
-        {
-            s_buildReport = report;
-        }
-        
-        [PostProcessBuild]
-        private static void SaveBuildInfo(BuildTarget target, string pathToBuiltProject)
-        {
-            //BuildPipeline.BuildPlayer()
-            //TypeCache.GetMethodsWithAttribute
-        }
+        private static string playerPath => EditorUserBuildSettings.GetBuildLocation(EditorUserBuildSettings.activeBuildTarget);
 
         [MenuItem("File/Launch Player %#&p", false, priority = 221)]
         private static void Launch()
         {
-#if UNITY_ANDROID
-            
-            string androidSdkRoot = EditorPrefs.GetString("AndroidSdkRoot");
-            string platformToolsPath = Path.Combine(androidSdkRoot, "platform-tools");
-            string adbPath = Path.Combine(platformToolsPath, "adb");
-            
-            var command = $"-d shell am start -n {Application.identifier}/com.unity3d.player.UnityPlayerActivity -a android.intent.action.MAIN -c android.intent.category.LAUNCHER";
+            var launcher = PlayerLauncherSettings.instance.launcherEntries[EditorUserBuildSettings.selectedBuildTargetGroup];
+            launcher.Launch(EditorUserBuildSettings.activeBuildTarget, playerPath);
+        }
+        
+        [MenuItem("File/Launch Player %#&p", true)]
+        private static bool ValidateLaunch()
+        {
+            if (string.IsNullOrEmpty(playerPath))
+                return false;
 
-            Process process = new Process();
-
-            var startInfo = new ProcessStartInfo
-            {
-                FileName = adbPath,
-                Arguments = command,
-                CreateNoWindow = true
-            };
-
-
-            process.StartInfo = startInfo;
-            process.Start();
-#endif
+            var buildTargetGroup = BuildPipeline.GetBuildTargetGroup(EditorUserBuildSettings.activeBuildTarget);
+            return PlayerLauncherSettings.instance.launcherEntries.TryGetLauncher(buildTargetGroup, out var launcher) &&
+                   launcher.CanLaunch(playerPath);
         }
     }
 }
